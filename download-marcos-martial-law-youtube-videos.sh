c872c406a3e06407dfb83aef463ecfa44b5b64e8
AnaralahBelore223 <105641675+AnaralahBelore223@users.noreply.github.com>
2022-05-16 23:13:08 +0800

    Add files via upload

---
+++ download-marcos-martial-law-youtube-videos.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+install_deb () {
+    apt-get install -y python3-pip python3
+}
+
+create_directory_if_not_exist () {
+    mkdir $HOME/Downloads/martial-law-videos
+}
+
+download_youtube_videos () {
+    cd $HOME/Downloads/martial-law-videos
+    yt-dlp -f 22 \
+    https://www.youtube.com/watch?v=3qHJ1WyQIWY \
+    https://www.youtube.com/watch?v=6DkcMF8cLNY \
+    https://www.youtube.com/watch?v=5gNujHs-Gho \
+    https://www.youtube.com/watch?v=H3AN7MCyOa4 \
+    https://www.youtube.com/watch?v=y9qkOLcpWJ4 \
+    https://www.youtube.com/watch?v=WnM2PxIILDk \
+
+}
+
+if [ ! -f /usr/local/bin/yt-dlp ]; then
+    pkexec pip3 install yt-dlp
+    if [ ! -f /usr/bin/pip3 ]; then
+        install_deb
+    fi
+    if [ ! -d $HOME/Downloads/martial-law-videos ]; then
+        create_directory_if_not_exist
+    fi
+fi
+create_directory_if_not_exist
+download_youtube_videos
